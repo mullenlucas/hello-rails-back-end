964ef69389327f9c2af54e44835d5cbc921fc8f9
mullenlucas <mullenlucas@gmail.com>
2023-01-24 11:32:30 -0300

    Update greeting messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: "-I solemnly swear that I am up to no good")
+Greeting.create(message: "-Mischief managed!")
+Greeting.create(message: "-I'm not crazy, my reality is just different than yours.")
+Greeting.create(message: "-We who are about to die, salute you! // Gladiators")
+Greeting.create(message: "-I am the one who knocks!")
+Greeting.create(message: "-I'm Batman")
+Greeting.create(message: "-I'm the king of the world!")
+Greeting.create(message: "-You had me at hello")
+Greeting.create(message: "-It is inevitable")
+Greeting.create(message: "-The Earth says 'Hello!'")
